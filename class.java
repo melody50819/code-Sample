1ee5bbeb342986db5b7a8fa2bcd02271cae5ebc0
melody50819 <melody870225@gmail.com>
2022-10-10 15:22:46 +0800

    Update class.java
    繼承 (Inheritance)
    多型 (Polymorphism)
    by 10/10

--- class.java
+++ class.java
@@ -2,6 +2,28 @@ class Animal {
     public String name;
 
     public String getFeatures() {...}
-    public String run() {...}
-    public String jump() {...}
+    public String run() {
+        return "I can run."
+    }
+    public String run(name) {
+        return "I am " + name + ", I can run."
+    }
+    public String jump() {
+        return "I can jump."
+    }
+}
+
+class Dog extends Animal{
+    public String run(name) {
+        return "I am " + name + ", I can run."
+    }
+
+}
+
+public static void main() {
+    Animal dog = new Animal("Dog");
+    Animal cat = new Animal("Cat");
+
+    dog.run();
+    cat.jump();
 }
